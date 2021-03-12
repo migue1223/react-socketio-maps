c5258108520dc0c361ea3805c746e351a79a23c0
desarrollo-opting <contabilidad@opting.co>
2021-03-12 17:21:46 -0500

    Codigo inicial

---
+++ maps-app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import { MapasApp } from './MapasApp';
+
+ReactDOM.render(<MapasApp />, document.getElementById('root'));
